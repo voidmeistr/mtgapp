cae44b61a0ef3333122fa30b35dfcd98a23648ed
Milan <voidmeistr@gmail.com>
2017-07-19 16:24:00 +0200

    added user account,login,logout,register

--- magic/urls.py
+++ magic/urls.py
@@ -4,6 +4,14 @@ from . import views
 app_name = 'magic'
 
 urlpatterns = [
+	#home page
 	url(r'^$', views.IndexView.as_view(), name='index'),
+	#registration page
+	url(r'^register/$', views.UserFormView.as_view(), name='register'),
+	#logout
+	url(r'^logout_user/$', views.logout_user, name='logout_user'),
+	#login
+    url(r'^login_user/$', views.login_user, name='login_user'),
+	#Post detail , /1/ ,/123/
 	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- magic/views.py
+++ magic/views.py
@@ -1,15 +1,80 @@
 from django.views import generic
 from .models import Post
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import View
+from .forms import UserForm
 # Create your views here.
 
-class IndexView(generic.ListView):
-	template_name = 'magic/index.html'
-	context_object_name = 'all_posts'
 
-	def get_queryset(self):
-		return Post.objects.all()
+class IndexView(generic.ListView):
+    template_name = 'magic/index.html'
+    context_object_name = 'all_posts'
+
+    def get_queryset(self):
+        return Post.objects.all()
+
 
 class DetailView(generic.DetailView):
-	model = Post
-	template_name = 'magic/detail.html'
+    model = Post
+    template_name = 'magic/detail.html'
+
+# registration page / registration itself
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'magic/registration_form.html'
+
+    # blank
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # data to process
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            # normalization of data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns USer object if everything is ok
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+
+                    login(request, user)
+                    return redirect('magic:index')
+
+        return render(request, self.template_name, {'form': form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                all_posts = Post.objects.all()
+                return render(request, 'magic/index.html', {'all_posts': all_posts})
+            else:
+                return render(request, 'magic/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'magic/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'magic/login.html')
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'magic/login.html', context)
